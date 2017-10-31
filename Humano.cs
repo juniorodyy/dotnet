372ed1f9cc54a6fb8447601b7444eda972e98e28
Junior Ody <junior.ody2000@gmail.com>
2017-10-31 19:30:10 -0200

    commit

--- Humano.cs
+++ Humano.cs
@@ -4,10 +4,20 @@ namespace console_dotnet
 {
     class Humano 
     {
-        public int randomicos(int min, int max)
+        public int randomicos(int a, int b)
         {
-          Random random = new Random();
-          return random.Next(min, max);   
+          Random t = new Random();
+          int j = t.Next(a, b);
+          int u = t.Next(a, b);
+          int soma = j + u;         
+          System.Console.WriteLine(j + "+" + r);
+
+          int resp = convert.ToInt32(Console_ReadLine()):
+
+          if (resp == soma) {
+              System.Console.WriteLine("Acesso liberado")
+          }
+
         }
     }
 }
